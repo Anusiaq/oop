70e1f79868460fb3b4f75b6b82ece2cb841bcb3e
Anna <anna.zareba@wp.eu>
2021-11-20 11:46:40 +0100

    opp

---
+++ Object_Programing/src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    public void startEngine(){
+        System.out.println("Silnik działa.");
+    }
+    public void stopEngine(){
+        System.out.println("Silnik nie działa");
+    }
+}
