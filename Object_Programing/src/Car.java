1a5bad16231f2c339ef66e2d22dcf1d6e9c0ec2b
Anna <anna.zareba@wp.eu>
2021-11-20 13:37:36 +0100

    opp

--- Object_Programing/src/Car.java
+++ Object_Programing/src/Car.java
@@ -1,6 +1,17 @@
 public class Car {
 
-    public int speed = 0;
+    private int speed = 0;
+    private int gear;
+    private String name;
+    private boolean lights;
+
+    public Car(int speed, int gear, String name, boolean lights){
+        this.speed = speed;
+        this.gear = gear;
+        this.name = name;
+        this.lights = lights;
+    }
+
 
 
     public void startEngine(){
@@ -17,5 +28,47 @@ public class Car {
     public void slowDown(int speed){
         this.speed -= speed;
         System.out.println("Samochód jedzie z prędkością " + this.speed + " km/h. ");
+    }
+    public void setGear(int gear){
+        if (gear < 7 && gear > 0){
+            this.gear = gear;
+            System.out.println("Jesteś na biegu " + this.gear);
+
+        }
+    }
+    public void setLights(boolean lights){
+        this.lights = lights;
+
+    }
+    public boolean getLights(){
+        return lights;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isLights() {
+        return lights;
+    }
+
+    public Car(){
+
     }
 }
--- Object_Programing/src/Main.java
+++ Object_Programing/src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car audi = new Car();
+        Car audi = new Car(0, 1, "audi", true);
         audi.startEngine();
+        audi.setLights(true);
+        System.out.println(audi.getLights());
+        audi.setGear(1);
         audi.accelrate( 20 );
         audi.accelrate(40);
         audi.slowDown(15);
         audi.stopEngine();
+
+        System.out.println("Car is change");
+
+        Car bmw = new Car(0, 1, "e36", true);
+        bmw.startEngine();
+        bmw.setLights(true);
+        System.out.println(bmw.getLights());
+        bmw.setGear(1);
+        bmw.accelrate( 20 );
+        bmw.accelrate(40);
+        bmw.slowDown(15);
+        bmw.stopEngine();
+
+    }
+
+
 
     }
 }
